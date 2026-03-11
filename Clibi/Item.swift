134ecad3be784ca90b61e151c26aa8168daed04b
Akinalp Fidan <81157056+akinalpfdn@users.noreply.github.com>
2026-03-11 18:12:33 +0300

    Initial Commit

---
+++ Clibi/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  Clibi
+//
+//  Created by Akinalp Fidan on 11.03.2026.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
